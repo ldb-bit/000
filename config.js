5bac09c5c398a5416222649dbb1a03fc31de7db1
ldb-bit <ldbb0903@gmail.com>
2026-04-01 10:13:02 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// ⚠ 이 파일은 .gitignore에 포함되어 깃허브에 올라가지 않습니다.
+// 절대 깃허브에 올리지 마세요.
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbx01uwjX9TGc6GMAhb-27jDGwjOhWJqTgtF-rQCP4YHV-_537Q04hHJ_jLCX9Rl3b2P/exec"
+};
